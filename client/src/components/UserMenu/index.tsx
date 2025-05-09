a0281d2d24e47a9ead681c02a98e47ff9ce2dda5
lalatina11 <rahmadancandra@gmail.com>
2025-05-09 22:55:19 +0700

    chore: create user menu ui

--- client/src/components/UserMenu/index.tsx
+++ client/src/components/UserMenu/index.tsx
@@ -1,5 +1,27 @@
+import { RxAvatar } from "react-icons/rx";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { buttonVariants } from "../ui/button";
+import UserDropDown from "./UserDropDown";
+
 const UserMenu = () => {
-  return <div>UserMenu</div>;
+  const currentUser = true;
+  return currentUser ? (
+    <div className={"flex gap-4 items-center"}>
+      <Avatar className="hidden md:block">
+        <AvatarImage src="https://avatars.githubusercontent.com/u/178248680?v=4" />
+        <AvatarFallback>
+          <RxAvatar />
+        </AvatarFallback>
+      </Avatar>
+      <UserDropDown />
+    </div>
+  ) : (
+    <div>
+      <a href="/" className={"" + buttonVariants({ variant: "default" })}>
+        Login
+      </a>
+    </div>
+  );
 };
 
 export default UserMenu;
