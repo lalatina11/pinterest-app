bdf07f0a0c5bc551cd16b43d17b08aef77447d63
lalatina11 <rahmadancandra@gmail.com>
2025-05-09 20:50:48 +0700

    chore: add ui for top bar's search

--- client/src/components/TopBar/index.tsx
+++ client/src/components/TopBar/index.tsx
@@ -1,5 +1,27 @@
+import { FaSearch } from "react-icons/fa";
+import { Input } from "../ui/input";
+import UserMenu from "../UserMenu";
+
 const TopBar = () => {
-  return <div>TopBar</div>;
+  return (
+    <div className="my-4 flex items-center gap-4">
+      {/* SEARCH */}
+      <div className="relative flex-1">
+        <button className="absolute top-3 left-4">
+          <FaSearch />
+        </button>
+        <Input
+          type="text"
+          className="px-12"
+          placeholder="Search.."
+          name=""
+          id=""
+        />
+      </div>
+      {/* USER */}
+      <UserMenu />
+    </div>
+  );
 };
 
 export default TopBar;
