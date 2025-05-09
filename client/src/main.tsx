191890d1dc9fc315f8e4f61b225f66db39e20383
lalatina11 <rahmadancandra@gmail.com>
2025-05-09 20:24:00 +0700

    style: default theme will be system theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
       <ToasterProvider />
     </ThemeProvider>
