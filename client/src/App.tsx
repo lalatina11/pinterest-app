59c5980557a5e7059af92477a42c874502d03aad
lalatina11 <rahmadancandra@gmail.com>
2025-05-09 22:56:35 +0700

    chore: add main ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import { toast } from "sonner";
-import { ModeToggle } from "./components/ModeToggle";
-import { Button } from "./components/ui/button";
+import Gallery from "./components/Gallery";
+import LeftBar from "./components/LeftBar";
+import TopBar from "./components/TopBar";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-      <ModeToggle />
-      <Button onClick={() => toast("OKK")}>Toast!</Button>
+    <div className="flex gap-4 w-full">
+      <LeftBar />
+      <div className="flex-1 mr-4">
+        <TopBar />
+        <Gallery />
+      </div>
     </div>
   );
 };
