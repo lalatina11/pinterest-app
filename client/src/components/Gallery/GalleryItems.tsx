bcb27f3c5131b2a92bd213f28f4b16554a6e6e56
lalatina11 <rahmadancandra@gmail.com>
2025-05-10 18:14:37 +0700

    feat: add gallery item overlay feature

--- client/src/components/Gallery/GalleryItems.tsx
+++ client/src/components/Gallery/GalleryItems.tsx
@@ -1,21 +1,41 @@
 import { noImageUrl } from "@/lib/dummyData";
 import type { TempItems } from "@/types";
+import { Link } from "react-router";
+import { CiCircleMore, CiSaveDown2 } from "react-icons/ci";
+import { FaShareAlt } from "react-icons/fa";
+import "./galleryItems.css";
 
 interface Props {
   item: TempItems;
+  itemId: number;
 }
 const GalleryItems = (props: Props) => {
-  const { item } = props;
+  const { item, itemId } = props;
   return (
     <div
-      className="flex hover:scale-110 transition-all ease-in-out duration-300 cursor-pointer"
+      className="relative flex transition-all ease-in-out duration-300 cursor-pointer galleryItem"
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
       <img
         src={item.imageUrl || noImageUrl}
         alt=""
-        className="w-full h-auto object-cover rounded"
+        className="w-full h-auto object-cover shadow shadow-zinc-500 rounded hover:rounded-xl"
       />
+      <Link
+        className="overlay absolute top-0 w-full h-full"
+        to={`/pin/${itemId}`}
+      />
+      <div className="overlayBtn gap-4 justify-between items-center bg-zinc-200 dark:bg-zinc-900">
+        <span className="hover:scale-125 p-1 lg:p-1.5 rounded-md bg-zinc-900 dark:bg-zinc-200 text-zinc-200 dark:text-zinc-900 transition-all ease-in-out duration-300">
+          <FaShareAlt />
+        </span>
+        <span className="hover:scale-125 p-1.5 rounded-md bg-zinc-900 dark:bg-zinc-200 text-zinc-200 dark:text-zinc-900 transition-all ease-in-out duration-300">
+          <CiSaveDown2 />
+        </span>
+        <span className="hover:scale-125 p-1.5 rounded-md bg-zinc-900 dark:bg-zinc-200 text-zinc-200 dark:text-zinc-900 transition-all ease-in-out duration-300">
+          <CiCircleMore />
+        </span>
+      </div>
     </div>
   );
 };
--- client/src/components/Gallery/index.tsx
+++ client/src/components/Gallery/index.tsx
@@ -5,7 +5,7 @@ const Gallery = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-4 auto-rows-[10px]">
       {tempItems.map((data, i) => (
-        <GalleryItems item={data} key={i} />
+        <GalleryItems item={data} itemId={i + 1} key={i} />
       ))}
     </div>
   );
