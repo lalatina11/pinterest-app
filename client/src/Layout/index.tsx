1f222d48b5ce0da96d4ec53e5b931cac53a87941
lalatina11 <rahmadancandra@gmail.com>
2025-05-10 08:52:04 +0700

    feat: layouting and routing

--- client/src/Layout/index.tsx
+++ client/src/Layout/index.tsx
@@ -1,5 +1,17 @@
+import LeftBar from "@/components/LeftBar";
+import TopBar from "@/components/TopBar";
+import { Outlet } from "react-router";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="flex gap-4 w-full">
+      <LeftBar />
+      <div className="flex-1 mr-4">
+        <TopBar />
+        <Outlet/>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,36 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import ToasterProvider from "./components/ToasterProvider.tsx";
 import "./global.css";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.tsx";
+import CreatePage from "./pages/CreatePage.tsx";
+import PostPage from "./pages/PostPage.tsx";
+import AuthPage from "./pages/AuthPage.tsx";
+import SearchPage from "./pages/SearchPage.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import Layout from "./Layout/index.tsx";
+import AuthLayout from "./Layout/AuthLayout.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <ToasterProvider />
-        <App />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/pin/:id" element={<PostPage />} />
+            <Route path="/:username" element={<ProfilePage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="/auth" element={<AuthPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>{" "}
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
