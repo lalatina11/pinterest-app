6f143b4a7562679a1dea81a4586416dab62a78ff
lalatina11 <rahmadancandra@gmail.com>
2025-05-10 20:40:24 +0700

    chore: add image io kit renderer compt

---
+++ client/src/components/Gallery/ImageRenderer.tsx
@@ -0,0 +1,30 @@
+import { noImageUrl } from "@/lib/dummyData";
+import { Image } from "@imagekit/react";
+
+interface Props {
+  imageUrl: string;
+  className?: string;
+  width?: number;
+  height?: number;
+  alt?: string;
+}
+
+const ImageRenderer = (props: Props) => {
+  const { imageUrl, className, width, height } = props;
+  return (
+    <Image
+      src={imageUrl ?? noImageUrl}
+      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_API || "your end point"}
+      className={
+        className ??
+        "w-full h-auto object-cover shadow shadow-zinc-500 rounded hover:rounded-xl"
+      }
+      loading="lazy"
+      transformation={[{ width: width ?? 372, height: height ?? undefined }]}
+      alt="Failed to upload the image"
+      lqip={{ active: true, quality: 20 }}
+    />
+  );
+};
+
+export default ImageRenderer;
