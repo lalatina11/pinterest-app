9c842b2f1b922ec37a1b06c2f9e93f48de982e2a
lalatina11 <rahmadancandra@gmail.com>
2025-05-10 08:41:18 +0700

    feat: create router for all pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import CreatePage from "./pages/CreatePage";
+import PostPage from "./pages/PostPage";
+import AuthPage from "./pages/AuthPage";
+import SearchPage from "./pages/SearchPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/create" element={<CreatePage />} />
+      <Route path="/pin/:id" element={<PostPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/:username" element={<ProfilePage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
