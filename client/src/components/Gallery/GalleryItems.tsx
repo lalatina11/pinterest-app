cc62bc3e670fae8f9b4220ec60ba352d6351300a
lalatina11 <rahmadancandra@gmail.com>
2025-05-10 20:41:02 +0700

    refactor: replace hard code Image renderer with image renderer reusability compt

--- client/src/components/Gallery/GalleryItems.tsx
+++ client/src/components/Gallery/GalleryItems.tsx
@@ -1,9 +1,9 @@
 import type { TempItems } from "@/types";
-import { Link } from "react-router";
 import { CiCircleMore, CiSaveDown2 } from "react-icons/ci";
 import { FaShareAlt } from "react-icons/fa";
+import { Link } from "react-router";
 import "./galleryItems.css";
-import { Image } from "@imagekit/react";
+import ImageRenderer from "./ImageRenderer";
 
 interface Props {
   item: TempItems;
@@ -16,16 +16,7 @@ const GalleryItems = (props: Props) => {
       className="relative flex transition-all ease-in-out duration-300 cursor-pointer galleryItem"
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
-      <Image
-        src={item.imageUrl}
-        urlEndpoint={import.meta.env.VITE_IMAGE_KIT_API || "your end point"}
-        className="w-full h-auto object-cover shadow shadow-zinc-500 rounded hover:rounded-xl"
-      />
-      {/* <img
-        src={item.imageUrl || noImageUrl}
-        alt=""
-        className="w-full h-auto object-cover shadow shadow-zinc-500 rounded hover:rounded-xl"
-      /> */}
+      <ImageRenderer imageUrl={item.imageUrl} />
       <Link
         className="overlay absolute top-0 w-full h-full"
         to={`/pin/${itemId}`}
