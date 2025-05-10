a60cc543293fd4a6f7396fe15ca0334cf974c435
lalatina11 <rahmadancandra@gmail.com>
2025-05-10 18:52:40 +0700

    feat: applied image kit io package libs

--- client/src/components/Gallery/GalleryItems.tsx
+++ client/src/components/Gallery/GalleryItems.tsx
@@ -1,9 +1,9 @@
-import { noImageUrl } from "@/lib/dummyData";
 import type { TempItems } from "@/types";
 import { Link } from "react-router";
 import { CiCircleMore, CiSaveDown2 } from "react-icons/ci";
 import { FaShareAlt } from "react-icons/fa";
 import "./galleryItems.css";
+import { Image } from "@imagekit/react";
 
 interface Props {
   item: TempItems;
@@ -16,11 +16,16 @@ const GalleryItems = (props: Props) => {
       className="relative flex transition-all ease-in-out duration-300 cursor-pointer galleryItem"
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
-      <img
+      <Image
+        src={item.imageUrl}
+        urlEndpoint={import.meta.env.VITE_IMAGE_KIT_API || "your end point"}
+        className="w-full h-auto object-cover shadow shadow-zinc-500 rounded hover:rounded-xl"
+      />
+      {/* <img
         src={item.imageUrl || noImageUrl}
         alt=""
         className="w-full h-auto object-cover shadow shadow-zinc-500 rounded hover:rounded-xl"
-      />
+      /> */}
       <Link
         className="overlay absolute top-0 w-full h-full"
         to={`/pin/${itemId}`}
