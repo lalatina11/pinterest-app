e30adbf7145147125fd6a8baf44fde28dd744c1e
lalatina11 <rahmadancandra@gmail.com>
2025-05-10 07:43:50 +0700

    style: add image hover scalling

--- client/src/components/Gallery/GalleryItems.tsx
+++ client/src/components/Gallery/GalleryItems.tsx
@@ -8,7 +8,7 @@ const GalleryItems = (props: Props) => {
   const { item } = props;
   return (
     <div
-      className="flex"
+      className="flex hover:scale-110 transition-all ease-in-out duration-300 cursor-pointer"
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
       <img
