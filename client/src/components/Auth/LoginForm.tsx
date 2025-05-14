d25b1368c5af9837e938a04ec35751c20c21fb6e
lalatina11 <rahmadancandra@gmail.com>
2025-05-14 14:25:23 +0700

    feat: add auth user feature ui

---
+++ client/src/components/Auth/LoginForm.tsx
@@ -0,0 +1,65 @@
+import { useTheme } from "@/lib/UseThemeContext";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Moon, Sun } from "lucide-react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+interface Props {
+  setType: React.Dispatch<
+    React.SetStateAction<"login" | "register" | "verify">
+  >;
+}
+
+const LoginForm = (props: Props) => {
+  const { setTheme } = useTheme();
+  return (
+    <Card className="Card w-sm max-w-sm flex justify-center items-center relative">
+      <CardHeader className="flex flex-col gap-3 justify-center items-center w-36 h-36">
+        <img
+          src="/public/logo-pin.svg"
+          className="w-full h-auto object-cover"
+        />
+        <CardTitle className="text-nowrap">
+          <span>Login to your account</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="w-full">
+        <span
+          onClick={() => setTheme("dark")}
+          className="block dark:hidden absolute top-5 right-5 cursor-pointer"
+        >
+          <Moon />
+        </span>
+        <span
+          onClick={() => setTheme("light")}
+          className="hidden dark:block absolute top-5 right-5 cursor-pointer"
+        >
+          <Sun />
+        </span>
+        <form className="space-y-6" action="">
+          <div className="space-y-3">
+            <Label>Email</Label>
+            <Input type="email" />
+          </div>
+          <div className="space-y-3">
+            <Label>Password</Label>
+            <Input type="password" />
+          </div>
+          <div className="flex gap-2 items-center">
+            <span>Don&apos;t have an Account?</span>
+            <span
+              className="text-blue-500 cursor-pointer"
+              onClick={() => props.setType("register")}
+            >
+              register here
+            </span>
+          </div>
+          <Button className="w-full">Login</Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default LoginForm;
--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,5 +1,18 @@
+import LoginForm from "@/components/Auth/LoginForm";
+import RegisterForm from "@/components/Auth/RegisterForm";
+import { useState } from "react";
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const [type, setType] = useState<"login" | "register" | "verify">("login");
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      {type === "login" ? (
+        <LoginForm setType={setType} />
+      ) : (
+        <RegisterForm setType={setType} />
+      )}
+    </div>
+  );
 };
 
 export default AuthPage;
