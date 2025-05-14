430828477bd7011c1142f40747c508e91cd4e025
lalatina11 <rahmadancandra@gmail.com>
2025-05-14 14:46:42 +0700

    style: change width and height main compt

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -5,13 +5,13 @@ import { useState } from "react";
 const AuthPage = () => {
   const [type, setType] = useState<"login" | "register" | "verify">("login");
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <main className="min-h-screen overflow-y-auto flex justify-center items-center">
       {type === "login" ? (
         <LoginForm setType={setType} />
       ) : (
         <RegisterForm setType={setType} />
       )}
-    </div>
+    </main>
   );
 };
 
