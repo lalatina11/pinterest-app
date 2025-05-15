0a9b5ade992b9b2cfa574188b6370648bfc72309
lalatina11 <rahmadancandra@gmail.com>
2025-05-15 20:08:07 +0700

    feat: apply all controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express"
 import userRoutes from "./routes/user"
+import pinRoutes from "./routes/pin"
+import commentRoutes from "./routes/comment"
+import boardRoute from "./routes/board"
 
 const PORT = process.env.PORT || 3030
 const app = express()
 app.use(express.json())
 
 app.use("/api/users", userRoutes)
+app.use("/api/pins", pinRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/boards", boardRoute)
 
 app.listen(PORT, () => {
     console.log(`Server up and Running on http://localhost:${PORT}`);
