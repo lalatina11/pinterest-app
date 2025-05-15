d3c79527cd8ea7d16ec21e906ae099f6522cb960
lalatina11 <rahmadancandra@gmail.com>
2025-05-15 18:18:42 +0700

    feat: setup express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import express from "express"
+
+const PORT = process.env.PORT || 3030
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`Server up and Running on http://localhost:${PORT}`);
+
+})
