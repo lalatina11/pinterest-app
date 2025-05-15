15091a1382f444fd32163837f93bbcd0cdac5546
lalatina11 <rahmadancandra@gmail.com>
2025-05-15 20:08:35 +0700

    refactor: rename comment controller varialble name

--- server/src/controller/comment.ts
+++ server/src/controller/comment.ts
@@ -1,9 +1,9 @@
 import asyncHandler from "../middlewares/asyncHandler";
 
-const commentsController = {
+const commentController = {
     create: asyncHandler(async (_, res) => {
         res.status(201).json({ message: "OK", error: false })
     })
 }
 
-export default commentsController
+export default commentController
