a41efaf5f2f85cb92179f80b4cc414083967bab1
lalatina11 <rahmadancandra@gmail.com>
2025-05-15 22:26:16 +0700

    feat: database designing

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -1,3 +1,4 @@
+import type mongoose from "mongoose";
 import type { Document } from "mongoose";
 
 export interface UserType extends Document {
@@ -6,6 +7,35 @@ export interface UserType extends Document {
     email: string;
     password?: string;
     avatar?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface PinType extends Document {
+    media: string;
+    width: number;
+    height: number;
+    title: string;
+    description: string;
+    link?: string;
+    board?: mongoose.Schema.Types.ObjectId;
+    tags?: string[];
+    user: mongoose.Schema.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface BoardType extends Document {
+    title: string;
+    user: mongoose.Schema.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface CommentType extends Document {
+    description: string;
+    pin: mongoose.Schema.Types.ObjectId;
+    user: mongoose.Schema.Types.ObjectId;
     createdAt: Date;
     updatedAt: Date;
 }
