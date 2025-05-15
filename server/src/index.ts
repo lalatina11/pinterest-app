52514b593c9233399cb38f3accc34fa7566d3fa1
lalatina11 <rahmadancandra@gmail.com>
2025-05-15 22:02:22 +0700

    feat: connecting db in port listening section

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,17 @@ import userRoutes from "./routes/user"
 import pinRoutes from "./routes/pin"
 import commentRoutes from "./routes/comment"
 import boardRoute from "./routes/board"
+import connectDB from "./config/db"
 
 const PORT = process.env.PORT || 3030
 const app = express()
 app.use(express.json())
-
 app.use("/api/users", userRoutes)
 app.use("/api/pins", pinRoutes)
 app.use("/api/comments", commentRoutes)
 app.use("/api/boards", boardRoute)
 
 app.listen(PORT, () => {
+    connectDB()
     console.log(`Server up and Running on http://localhost:${PORT}`);
-
 })
