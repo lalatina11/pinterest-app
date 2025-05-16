fb1c3c3c995d9f495cf9e936773beff4e1ec7781
lalatina11 <rahmadancandra@gmail.com>
2025-05-16 09:57:09 +0700

    feat: apply cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,9 +5,14 @@ import commentRoutes from "./routes/comment"
 import boardRoute from "./routes/board"
 import connectDB from "./config/db"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 const PORT = process.env.PORT || 3030
 const app = express()
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/users", userRoutes)
