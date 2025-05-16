83a3231a33ff3a6eafe88986aa7bb91cf4ca1c3b
lalatina11 <rahmadancandra@gmail.com>
2025-05-16 18:53:40 +0700

    feat: apply login user api

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -3,5 +3,6 @@ import userController from "../controller/user"
 const route = express()
 
 route.post("/register", userController.register)
+route.post("/login", userController.login)
 
 export default route
