4b62a37bbf7cdc35e0c174403aa063fb4ddae920
lalatina11 <rahmadancandra@gmail.com>
2025-05-16 19:53:37 +0700

    feat: add controller and router for get all pins end point

--- server/src/controller/pin.ts
+++ server/src/controller/pin.ts
@@ -1,8 +1,13 @@
 import asyncHandler from "../middlewares/asyncHandler";
+import Pin from "../models/pin";
 
 const pinController = {
     create: asyncHandler(async (_, res) => {
         res.status(201).json({ message: "OK", error: false })
+    }),
+    getAallPins: asyncHandler(async (_, res) => {
+        const pins = await Pin.find()
+        res.status(200).send({ message: "OK", data: pins, error: false })
     })
 }
 
--- server/src/routes/pin.ts
+++ server/src/routes/pin.ts
@@ -2,6 +2,7 @@ import express from "express"
 import pinController from "../controller/pin"
 const route = express()
 
-route.post("/create", pinController.create)
+route.post("/", pinController.create)
+route.get("/", pinController.getAallPins)
 
 export default route
