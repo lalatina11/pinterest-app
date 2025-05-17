a76128669a456d1264c74159fecfedd4264cb7c7
lalatina11 <rahmadancandra@gmail.com>
2025-05-17 08:12:00 +0700

    feat: get data in single page

---
+++ client/src/lib/index.ts
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+export const apiRequest = axios.create({ baseURL: import.meta.env.VITE_API_KEY || "http://localhost:3030" })
--- client/src/pages/PostPage.tsx
+++ client/src/pages/PostPage.tsx
@@ -5,10 +5,30 @@ import ImageRenderer from "@/components/Gallery/ImageRenderer";
 import Metadata from "@/components/Metadata";
 import PostInterraction from "@/components/PostInterraction";
 import Avatar from "@/components/UserMenu/Avatar";
+import { apiRequest } from "@/lib";
+import { useQuery } from "@tanstack/react-query";
 import { NavLink, useParams } from "react-router";
+import NotFoundPage from "./NotFoundPage";
 
 const PostPage = () => {
   const { id } = useParams();
+
+  const { data, error, isPending } = useQuery({
+    queryKey: ["pin", id],
+    queryFn: async () => {
+      const { data } = await apiRequest.get(`/api/pins/${id}`);
+      return data.data;
+    },
+  });
+
+  if (isPending) {
+    return <h1 className="flex justify-center items-center">Loading...</h1>;
+  }
+  if (error) {
+    return NotFoundPage();
+  }
+
+  if (!data) return NotFoundPage();
 
   return (
     <>
@@ -20,7 +40,7 @@ const PostPage = () => {
           </div>
           <div className="flex-1/3 lg:flex-1/3">
             <ImageRenderer
-              imageUrl={`/Pins/pin${id}.jpeg`}
+              imageUrl={data.media}
               className="w-full m-auto rounded-md"
               width={736}
             />
@@ -28,11 +48,11 @@ const PostPage = () => {
           <div className="flex flex-col gap-4 flex-1/3">
             <PostInterraction />
             <NavLink
-              to={"/profile/johndoe"}
+              to={"/profile/" + data.user.username}
               className="flex gap-2 items-center w-fit"
             >
-              <Avatar avatarUrl="" className="w-8 h-8" />
-              <span className="text-sm lg:text-lg">John Doe</span>
+              <Avatar avatarUrl={data.user.avatar} className="w-8 h-8" />
+              <span className="text-sm lg:text-lg">{data.user.name}</span>
             </NavLink>
             <div className="w-full h-[1px] bg-zinc-500" />
             <Comments />
