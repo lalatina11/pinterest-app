6f67f84bb5a9d4b2ee5a941afbb70f3d06368686
lalatina11 <rahmadancandra@gmail.com>
2025-05-20 07:32:27 +0700

    feat: add verify otp section

---
+++ client/src/components/Auth/VerifyForm.tsx
@@ -0,0 +1,62 @@
+import { useEffect, type FormEventHandler } from "react";
+import { toast } from "sonner";
+import { Button } from "../ui/button";
+import { Card, CardContent } from "../ui/card";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "../ui/input-otp";
+import { Label } from "../ui/label";
+
+interface Props {
+  setType: React.Dispatch<
+    React.SetStateAction<"login" | "register" | "verify">
+  >;
+}
+
+const VerifyForm = (props: Props) => {
+  const { setType } = props;
+  const handleSubmitForm: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    toast(new FormData(e.currentTarget).get("OTP")?.toString());
+  };
+
+  useEffect(() => {
+    if (!sessionStorage.getItem("identifierUser")) {
+      setType("login");
+    }
+  }, [setType]);
+
+  return (
+    <Card className="Card">
+      <span className="flex text-xl font-semibold w-fit m-auto">
+        Verify Your Registry
+      </span>
+      <CardContent>
+        <form onSubmit={handleSubmitForm} className="max-w-sm space-y-3">
+          <div className="flex justify-center items-center flex-col gap-3">
+            <Label>Your OTP Code</Label>
+            <InputOTP name="OTP" maxLength={6}>
+              <InputOTPGroup>
+                <InputOTPSlot index={0} />
+                <InputOTPSlot index={1} />
+                <InputOTPSlot index={2} />
+              </InputOTPGroup>
+              <InputOTPSeparator />
+              <InputOTPGroup>
+                <InputOTPSlot index={3} />
+                <InputOTPSlot index={4} />
+                <InputOTPSlot index={5} />
+              </InputOTPGroup>
+            </InputOTP>
+          </div>
+          <Button className="w-full">Submit</Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default VerifyForm;
--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,5 +1,6 @@
 import LoginForm from "@/components/Auth/LoginForm";
 import RegisterForm from "@/components/Auth/RegisterForm";
+import VerifyForm from "@/components/Auth/VerifyForm";
 import { useState } from "react";
 
 const AuthPage = () => {
@@ -8,8 +9,10 @@ const AuthPage = () => {
     <main className="min-h-screen overflow-y-auto flex justify-center items-center">
       {type === "login" ? (
         <LoginForm setType={setType} />
-      ) : (
+      ) : type === "register" ? (
         <RegisterForm setType={setType} />
+      ) : (
+        <VerifyForm setType={setType} />
       )}
     </main>
   );
