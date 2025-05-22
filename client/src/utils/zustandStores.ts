fdd4d449b47ef5e44429f3dddf7374cad495fd25
lalatina11 <rahmadancandra@gmail.com>
2025-05-22 22:50:18 +0700

    feat: add auth store user from zustand to manage user auth store

---
+++ client/src/utils/zustandStores.ts
@@ -0,0 +1,14 @@
+import type { User } from "@/types"
+import { create } from "zustand"
+type AuthStore = {
+    currentUser: User | null,
+    setCurrentUser: (newUser: User) => void
+    removeCurrentUser: () => void
+    updateCurrentUser: (newUser: User) => void
+}
+export const useAuthStore = create<AuthStore>((set) => ({
+    currentUser: null,
+    setCurrentUser: (newUser: User) => set({ currentUser: newUser }),
+    removeCurrentUser: () => set({ currentUser: null }),
+    updateCurrentUser: (updatedUser: User) => set({ currentUser: updatedUser }),
+}))
