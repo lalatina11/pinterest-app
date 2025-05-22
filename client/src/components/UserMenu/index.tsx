9cbad65a7af72bc1513a09429731a6705a66bd63
lalatina11 <rahmadancandra@gmail.com>
2025-05-22 23:53:19 +0700

    feat: add login button on top bar when there are no user logged in

--- client/src/components/UserMenu/index.tsx
+++ client/src/components/UserMenu/index.tsx
@@ -2,20 +2,21 @@ import { Link } from "react-router";
 import { buttonVariants } from "../ui/button";
 import UserDropDown from "./UserDropDown";
 import Avatar from "./Avatar";
+import { useAuthStore } from "@/utils/zustandStores";
 
 const UserMenu = () => {
-  const currentUser = true;
+  const { currentUser } = useAuthStore();
   return currentUser ? (
     <div className={"flex gap-4 items-center"}>
-      <Avatar
-        className="hidden md:block"
-        avatarUrl="https://avatars.githubusercontent.com/u/178248680?v=4"
-      />
+      <Avatar className="hidden md:block" avatarUrl={currentUser.avatar} />
       <UserDropDown />
     </div>
   ) : (
     <div>
-      <Link to="/" className={"" + buttonVariants({ variant: "default" })}>
+      <Link
+        to="/auth?type=login"
+        className={"" + buttonVariants({ variant: "default" })}
+      >
         Login
       </Link>
     </div>
