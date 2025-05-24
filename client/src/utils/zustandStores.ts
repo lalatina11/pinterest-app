39c64d111aff17a5efd5de087c4ec8e456c9a589
lalatina11 <rahmadancandra@gmail.com>
2025-05-24 13:22:19 +0700

    refactor: delete unnecessary code

--- client/src/utils/zustandStores.ts
+++ client/src/utils/zustandStores.ts
@@ -1,4 +1,4 @@
-import type { Comment, User } from "@/types"
+import type { User } from "@/types"
 import { create } from "zustand"
 type AuthStore = {
     currentUser: User | null,
@@ -11,16 +11,4 @@ export const useAuthStore = create<AuthStore>((set) => ({
     setCurrentUser: (newUser: User) => set({ currentUser: newUser }),
     removeCurrentUser: () => set({ currentUser: null }),
     updateCurrentUser: (updatedUser: User) => set({ currentUser: updatedUser }),
-}))
-
-type CommentStore = {
-    comment: Comment[]
-    setComment: (comment: Comment) => void
-    setComments: (comments: Comment[]) => void
-}
-
-export const useCommentStore = create<CommentStore>((set) => ({
-    comment: [],
-    setComment: (newComment: Comment) => set((state) => ({ comment: [newComment, ...state.comment] })),
-    setComments: (comments: Comment[]) => set({ comment: comments })
 }))
